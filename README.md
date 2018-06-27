5e6c9b0884adb05c81a4503a82f79520297c5feb
astak16 <1500846601@qq.com>
2018-06-27 20:41:46 +0800

    倒计时

--- README.md
+++ README.md
@@ -28,5 +28,6 @@
 4. [输入两个数字，比较大小](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E8%BE%93%E5%85%A5%E4%B8%A4%E4%B8%AA%E6%95%B0%E5%AD%97%EF%BC%8C%E6%AF%94%E8%BE%83%E5%A4%A7%E5%B0%8F/index.html)
 5. [判读数字是否是两位数](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E5%88%A4%E6%96%AD%E6%95%B0%E5%AD%97%E5%87%A0%E4%BD%8D%E6%95%B0/index.html)
 6. [页面加载后累加，自加1](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E9%A1%B5%E9%9D%A2%E5%8A%A0%E8%BD%BD%E5%90%8E%E7%B4%AF%E5%8A%A0%EF%BC%8C%E8%87%AA%E5%8A%A01/index.html)
-6. [网页计算器](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E7%BD%91%E9%A1%B5%E8%AE%A1%E7%AE%97%E5%99%A8/index.html)
+7. [网页计算器](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E7%BD%91%E9%A1%B5%E8%AE%A1%E7%AE%97%E5%99%A8/index.html)
+8. [简易网页时钟](https://astak16.github.io/Study-JS/%E7%AC%AC%E4%B8%89%E8%AF%BE/%E7%AE%80%E6%98%93%E7%BD%91%E9%A1%B5%E5%80%92%E8%AE%A1%E6%97%B6/index.html)
 
---
+++ 第三课/倒计时/js/main.js
@@ -0,0 +1,50 @@
+
+var span = document.getElementsByTagName('span')
+var input = document.getElementsByTagName('input')
+var id
+input[0].addEventListener('click',function () {
+
+    switch (this.value){
+        case '启动':
+            id = setInterval(updateTime,100)
+            this.value = '取消'
+            break;
+        case '取消':
+            clearInterval(id)
+            this.value = '启动'
+            break;
+        default:
+            alert('错误');
+    }
+})
+
+function format(a) {
+    console.log(a.toString().replace(/^(\d)$/,'$1'))
+    return a.toString().replace(/^(\d)$/,'0$1')
+}
+
+function updateTime(){
+    var spanText1 = span[0].innerHTML
+    var spanText2 = span[1].innerHTML
+    var remain = spanText1.replace(/^0/,'')*60 + parseInt(spanText2.replace(/^0/,''))
+
+    if(remain <= 0){
+        // console.log(remain)
+        clearInterval(id)
+        return
+    }
+
+    --remain;
+    // console.log('--remain')
+    // console.log(remain)
+    span[0].innerHTML = format(parseInt(remain / 60));
+    // console.log(span[0].innerHTML)
+    remain %= 60;
+    // console.log('remain%')
+    // console.log(remain)
+
+    span[1].innerHTML = format(parseInt(remain))
+    // console.log(span[1].innerHTML)
+
+}
+
